e735e00b3dc110a420ec732b5c05462fbb381199
asadtanvirshami <asadworkemail@gmail.com>
2024-09-14 08:27:14 +0500

    error resolve

--- src/atoms/userAtom/index.ts
+++ src/atoms/userAtom/index.ts
@@ -1,18 +1,29 @@
 import { atom } from "jotai";
 import Cookies from "js-cookie";
 
-// Initialize user data from cookies or set default values
+// Initialize user data from cookies or set default values with safe parsing
 const userDataCache = Cookies.get("user");
-const initialUserData = userDataCache
-  ? JSON.stringify(userDataCache)
-  : {
-      user: null,
-    };
-// Atom to store user data with persistence
-export const userAtom = atom(initialUserData, (get, set, newUserData: any) => {
-  // Update the atom value
-  set(userAtom, newUserData);
 
-  // Update cookies
-  Cookies.set("user", newUserData);
-});
+let initialUserData;
+
+try {
+  initialUserData = userDataCache ? JSON.parse(userDataCache) : { user: null };
+} catch (error) {
+  console.error("Error parsing user data from cookies:", error);
+  initialUserData = { user: null }; // Set default in case of error
+}
+
+// Atom to store user data
+export const userAtom = atom(initialUserData);
+
+// Function to update the atom and cookies
+export const updateUserAtom = atom(
+  null,
+  (get, set, newUserData: any) => {
+    // Update the atom value
+    set(userAtom, newUserData);
+
+    // Update cookies
+    Cookies.set("user", JSON.stringify(newUserData));
+  }
+);
